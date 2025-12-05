4c219e5b0ec3b8167ab33195fed85a6210d00111
huenathena-design <huenathena@gmail.com>
2025-12-05 17:30:08 -0500

    Add files via upload

---
+++ subsystems.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+
+extern Drive chassis;
+
+// Your motors, sensors, etc. should go here.  Below are examples
+
+// inline pros::Motor intake(1);
+// inline pros::adi::DigitalIn limit_switch('A');
+inline pros::Motor intake(-1);
+inline pros::Motor outtake(2);
+inline pros::Motor basket(3);
+inline ez::Piston scrapper('A');
+inline ez::Piston hood('B');
+inline pros::Distance distancesensor(13);
