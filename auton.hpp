d8441e3434f08e837b0dfd94787a57439a6510cd
huenathena-design <huenathena@gmail.com>
2026-04-14 18:31:02 -0400

    Refactor auton.hpp function declarations

---
+++ auton.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+void default_constants();
+
+void red_right();
+void red_right();
+void red_left();
+void red_left();
+void blue_right();
+void blue_left();
+void Skills();
+void Backup_Skills();
+void turn_example()
