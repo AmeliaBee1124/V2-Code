75d79dc254be999aa3601751f42430073dea3aad
huenathena-design <huenathena@gmail.com>
2026-04-14 18:33:49 -0400

    Update and rename subsystem.hpp V4 to subsystem.hpp

--- subsystem.hpp
+++ subsystem.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
-#include "EZ-Template/api.hpp"
+#include "api.hpp"
 #include "api.h"
 
 extern Drive chassis;
 
-// Your motors, sensors, etc. should go here.  Below are examples
+// motors, sensors, etc.
 
 // inline pros::Motor intake(1);
 // inline pros::adi::DigitalIn limit_switch('A');
